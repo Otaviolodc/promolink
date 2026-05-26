2c12cb3d62dcf3adffc4b51ba7bf04d924568a6c
Otaviolodc <tavinholodc10@gmail.com>
2026-05-25 21:10:08 -0300

    fix webhook auth

--- src/app/api/asaas/webhook/route.ts
+++ src/app/api/asaas/webhook/route.ts
@@ -1,45 +1,12 @@
 import { NextResponse } from "next/server";
 
-import { supabase } from "@/lib/supabase";
-
 export async function POST(req: Request) {
 
   try {
 
     const body = await req.json();
 
-    console.log("WEBHOOK:");
-    console.log(body);
-
-    const event = body.event;
-
-    // pagamento confirmado
-    if (
-      event === "PAYMENT_RECEIVED"
-      ||
-      event === "PAYMENT_CONFIRMED"
-    ) {
-
-      const payment =
-        body.payment;
-
-      const userId =
-        payment.externalReference;
-
-      console.log("USER ID:");
-      console.log(userId);
-
-      // ativa PRO
-      await supabase
-        .from("profiles")
-        .update({
-          is_pro: true,
-        })
-        .eq("id", userId);
-
-      console.log("PRO ATIVADO");
-
-    }
+    console.log("Webhook recebido:", body);
 
     return NextResponse.json({
       success: true,
