060e1103d57f6e9de40c590b1bcb5c5858244b38
Otaviolodc <tavinholodc10@gmail.com>
2026-05-25 21:20:01 -0300

    add webhook pro unlock

--- src/app/api/asaas/webhook/route.ts
+++ src/app/api/asaas/webhook/route.ts
@@ -1,5 +1,12 @@
 import { NextResponse } from "next/server";
 
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
+);
+
 export async function POST(req: Request) {
 
   try {
@@ -7,6 +14,35 @@ export async function POST(req: Request) {
     const body = await req.json();
 
     console.log("Webhook recebido:", body);
+
+    // pagamento confirmado
+    if (
+      body.event === "PAYMENT_CONFIRMED"
+    ) {
+
+      const email =
+        body.payment.customer.email;
+
+      console.log(
+        "Liberando PRO para:",
+        email
+      );
+
+      const { error } =
+        await supabase
+          .from("profiles")
+          .update({
+            is_pro: true,
+          })
+          .eq("email", email);
+
+      if (error) {
+
+        console.log(error);
+
+      }
+
+    }
 
     return NextResponse.json({
       success: true,
